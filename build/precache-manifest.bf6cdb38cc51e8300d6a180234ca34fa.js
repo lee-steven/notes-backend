fc3c1e6cb1f8c0386a632d455d9ea60f4ac9dd25
Steven Lee <lee.steven@wustl.edu>
2020-05-14 18:57:46 -0700

    Added static page

---
+++ build/precache-manifest.bf6cdb38cc51e8300d6a180234ca34fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1b38445eec109ba22741301104e2e81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e9a3238fbbfefb2923e",
+    "url": "/static/css/main.ab7582b6.chunk.css"
+  },
+  {
+    "revision": "aeaf239959a527647721",
+    "url": "/static/js/2.efe66caf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.efe66caf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e9a3238fbbfefb2923e",
+    "url": "/static/js/main.3af49ebe.chunk.js"
+  },
+  {
+    "revision": "a87961645e06e6986bca",
+    "url": "/static/js/runtime-main.bd899a84.js"
+  }
+]);
